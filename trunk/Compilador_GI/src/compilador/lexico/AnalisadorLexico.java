29738e7cfa0c5a92040b832b90b4f2cd295d2f72
gabriel.ecomp <gabriel.ecomp@bc9af476-0552-8d74-5513-c5d957e21824>
2011-10-27 13:02:22 +0000

    começando automato

--- trunk/Compilador_GI/src/compilador/lexico/AnalisadorLexico.java
+++ trunk/Compilador_GI/src/compilador/lexico/AnalisadorLexico.java
@@ -6,6 +6,10 @@
 package compilador.lexico;
 
 import compilador.gui.Janela;
+import compilador.token.Token;
+import compilador.token.TokenType;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -15,9 +19,15 @@ public class AnalisadorLexico {
 
     Janela janela;
 
+    List<Token> tokens = new ArrayList<Token>();
+
     public AnalisadorLexico()
     {
         janela = new Janela();
         janela.setVisible(true);
     }
+
+    
+
+
 }
---
+++ trunk/Compilador_GI/src/compilador/lexico/Estado.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package compilador.lexico;
+
+/**
+ *
+ * @author Gabriel
+ */
+public enum Estado {
+
+    START,
+    EM_ID,
+    EM_NUM,
+    FIM
+
+
+}
