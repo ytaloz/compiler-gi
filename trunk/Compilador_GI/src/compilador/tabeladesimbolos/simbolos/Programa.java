b4e9ad72027d9c7f33f02372641d2de0357633ab
gabriel.ecomp <gabriel.ecomp@bc9af476-0552-8d74-5513-c5d957e21824>
2012-03-15 22:51:57 +0000

    criando classes pra representar variaveis, clases, etc

---
+++ trunk/Compilador_GI/src/compilador/tabeladesimbolos/simbolos/Programa.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package compilador.tabeladesimbolos.simbolos;
+
+import compilador.tabeladesimbolos.Escopo;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Programa extends Escopo {
+
+    //contantes globais
+    private HashMap<String,Constante> constantes = new HashMap<String,Constante>();
+
+    //variaveis globais
+    private HashMap<String,Variavel> variaveis = new HashMap<String,Variavel>();
+
+    //classes do programa
+    private HashMap<String,Classe> classes = new HashMap<String,Classe>();
+
+
+    public Programa()
+    {
+        super(null);
+    }
+}
